cc27b7b719304118e85ef896cd717cac87ec80e6
Archana Deshpal <archana.deshpal@gmail.com>
2026-04-18 13:17:03 +0530

    Nudge hero section slightly upward

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ export default function Hero() {
         ))}
       </div>
 
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-10 pb-12">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-12">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
